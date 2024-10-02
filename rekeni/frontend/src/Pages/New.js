24d946177932a2ffb7afa66e246e532abfc90530
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-10-02 22:49:28 +0200

    crreating code space for pages

---
+++ rekeni/frontend/src/Pages/New.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function New() {}
+
+export default New;
---
+++ rekeni/frontend/src/Pages/SelectTopAlbums.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function SelectTopAlbums() {}
+
+export default SelectTopAlbums;
