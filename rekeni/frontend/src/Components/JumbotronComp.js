491d6a17751f77b312d29eca6b55609416a668da
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-10-03 01:59:50 +0200

    Home page components skellies

---
+++ rekeni/frontend/src/Components/JumbotronComp.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+function JumbotronComp() {
+  <>
+    <Row>
+      <Col>
+        <h1>REKENI</h1>
+      </Col>
+      <Col>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+        blanditiis adipisci aspernatur beatae quibusdam fugiat minus aliquid
+        molestias, eveniet facere illo possimus doloremque odio sint repudiandae
+        excepturi rem. Ducimus, omnis!
+      </Col>
+    </Row>
+  </>;
+}
+
+export default JumbotronComp;
--- rekeni/frontend/src/Pages/Home.js
+++ rekeni/frontend/src/Pages/Home.js
@@ -1,5 +1,37 @@
 import React from "react";
 
-function Home() {}
+import { Col, Row, Container } from "react-bootstrap";
+
+import NavbarComp from "../Components/NavbarComp";
+import JumbotronComp from "../Components/JumbotronComp";
+import ReviewcCardComp from "../Components/ReviewCardComp";
+import AlbumCaroComp from "../Components/AlbumCaroComp";
+import FooterComp from "../Components/FooterComp";
+
+import "../Styles/main.css";
+
+function Home() {
+  return (
+    <Container fluid="md">
+      <Row>
+        <Col xs={12}>
+          <NavbarComp />
+        </Col>
+        <Col xs={12}>
+          <JumbotronComp />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6}>
+          <ReviewcCardComp />
+        </Col>
+        <Col xs={6}>
+          <AlbumCaroComp />
+        </Col>
+      </Row>
+      <FooterComp />
+    </Container>
+  );
+}
 
 export default Home;
