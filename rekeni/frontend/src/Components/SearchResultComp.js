8895d7d14344df9c4f71514f78e42a4d4da2ad43
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-10-16 14:12:42 +0200

    changing cards

--- rekeni/frontend/src/Components/SearchResultComp.js
+++ rekeni/frontend/src/Components/SearchResultComp.js
@@ -7,78 +7,26 @@ import "../Styles/ComponentStyles/search-results.css";
 import placeholderImg from "../Asset/pexels-scenicspire-358690216-28216688.jpg";
 
 function SearchResultComp() {
-  const albums = [
-    {
-      title: "Album 1",
-      artist: "Artist 1",
-      releaseDate: "2023",
-      image: placeholderImg, // Placeholder for album image
-    },
-    {
-      title: "Album 2",
-      artist: "Artist 2",
-      releaseDate: "2022",
-      image: placeholderImg,
-    },
-    {
-      title: "Album 3",
-      artist: "Artist 3",
-      releaseDate: "2021",
-      image: placeholderImg,
-    },
-    {
-      title: "Album 4",
-      artist: "Artist 4",
-      releaseDate: "2020",
-      image: placeholderImg,
-    },
-    {
-      title: "Album 5",
-      artist: "Artist 5",
-      releaseDate: "2019",
-      image: placeholderImg,
-    },
-  ];
-
   return (
-    <Container className="mindmap-container">
-      <Row>
-        <Col xs={12}>
-          {/* Central node */}
-          <div className="central-node">
-            <div className="d-flex justify-content-center align-items-center text-align-center py-4">
-              <img
-                className="searched-album"
-                src={placeholderImg}
-                alt="searched album"
-              />
-            </div>
+    <>
+      <Row className="results-search-contain">
+        <Col className="result-col col-12 d-flex align-content-center">
+          <div className="img-sml">
+            <img
+              className="result-img-search"
+              src={placeholderImg}
+              alt="Rendered Album from resluts"
+            />
           </div>
-
-          {/* Map over the albums array to create nodes with Card components */}
-          {albums.map((album, index) => (
-            <Card
-              key={index}
-              className={`mindmap-node node-${
-                index + 1
-              } node d-flex justify-content-center align-items-center`}
-            >
-              <Card.Img
-                className="image-discover"
-                variant="top"
-                src={album.image}
-                alt={album.title}
-              />
-              <Card.Body>
-                <Card.Title>{album.title}</Card.Title>
-                <Card.Text>By: {album.artist}</Card.Text>
-                <Card.Text>Release Date: {album.releaseDate}</Card.Text>
-              </Card.Body>
-            </Card>
-          ))}
+          <div className="album-meta-res">
+            <h5 className="title">Albulm Title</h5>
+            <h6 className="name">Artist Name</h6>
+            <br />
+            <p className="year">Release Year: 0000</p>
+          </div>
         </Col>
       </Row>
-    </Container>
+    </>
   );
 }
 
